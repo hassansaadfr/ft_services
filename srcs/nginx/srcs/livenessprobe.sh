40d9459ae76a9145d72845d8a401536b5c233cf6
hassansaadfr <saadaoui.hassan@laposte.net>
2021-03-24 11:33:39 +0100

    correct

--- srcs/nginx/srcs/livenessprobe.sh
+++ srcs/nginx/srcs/livenessprobe.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#!/bin/sh
-
 # check if telegraf is running
 if ! pgrep -x "telegraf" > /dev/null
 then
@@ -11,13 +9,11 @@ fi
 # check if nginx is running
 if ! pgrep "nginx" > /dev/null
 then
-        echo "Nginx DOWN"
         exit 1
 fi
 
 # check if ssh is running
 if ! pgrep "ssh" >/dev/null
 then
-        echo "ssh UP"
         exit 1
 fi
